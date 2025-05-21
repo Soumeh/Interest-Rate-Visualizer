20b28afdb813427d13bcba1d14f0513582e47266
CreationsMarko <creationsmarko@gmail.com>
2025-05-21 20:48:26 +0200

    improve frontend

---
+++ src/common/__init__.py
@@ -0,0 +1,15 @@
+
+month_names = {
+    1: "Januar",
+    2: "Februar",
+    3: "Mart",
+    4: "April",
+    5: "Maj",
+    6: "Jun",
+    7: "Jul",
+    8: "Avgust",
+    9: "Septembar",
+    10: "Oktobar",
+    11: "Novembar",
+    12: "Decembar",
+}
